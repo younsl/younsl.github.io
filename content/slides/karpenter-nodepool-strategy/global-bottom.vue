bd8fff5054df649bfa310022900999bfd36b853e
younsl <cysl@kakao.com>
2025-06-07 07:55:32 +0900

    clean: Delete migrated slide to article

--- content/slides/karpenter-nodepool-strategy/global-bottom.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <footer
-    v-if="$nav.currentLayout !== 'cover'"
-    class="absolute bottom-0 right-0 p-2 text-sm opacity-50"
-  >
-    {{ $nav.currentPage }} / {{ $nav.total }}
-  </footer>
-</template>
